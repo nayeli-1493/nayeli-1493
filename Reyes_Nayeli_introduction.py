68349474352b666f6b34ef5ddf3a744e150a8831
nayeli-1493 <r.nayely408@yahoo.com>
2024-09-05 20:23:34 -0700

    Create Reyes_Nayeli_introduction.py

---
+++ Reyes_Nayeli_introduction.py
@@ -0,0 +1,9 @@
+print("Hello, my name is Nayeli") #Python print() to output my name
+print("My favorite color is pink") #favorite color
+print("My favorite word is "grinchy"") #favorite word
+print("") #blank line 
+print("My favorite peom:") #print() to output favorite peom
+print("Indays gone by when we were broken")
+print("We hated to reaveal it")
+print("We struggle to conceal it.") 
+print("    - Anonymous")
